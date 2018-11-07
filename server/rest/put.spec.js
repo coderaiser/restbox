b81c39b72b6514d50c727fce1b3750a83110c188
coderaiser <coderaiser@cloudcmd.io>
2018-11-07 13:03:26 +0200

    test(put) add

---
+++ server/rest/put.spec.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const calledWithDiff = require('sinon-called-with-diff');
+const sinon = calledWithDiff(require('sinon'));
+const test = require('tape');
+const through2 = require('through2');
+
+const mockRequire = require('mock-require');
+const {reRequire} = mockRequire;
+
+const restbox = require('../restbox');
+const {request} = require('serve-once')(restbox);
+
+const pathPut = './put';
+const pathRestbox = '../restbox';
+
+test('restbox: put: no token', async (t) => {
+    const {body} = await request.put('/dropbox/fs/temp');
+    
+    t.equal(body, 'token should be a string!', 'should return error message');
+    t.end();
+});
+
+test('restbox: put: mkdir', async (t) => {
+    const mkdir = sinon
+        .stub()
+        .returns(Promise.resolve());
+    
+    mockRequire('@cloudcmd/dropbox', {
+        mkdir,
+    });
+    
+    reRequire(pathPut);
+    const restbox = reRequire(pathRestbox);
+    
+    const {request} = reRequire('serve-once')(restbox);
+    
+    const token = 'hello'
+    const options = {
+        token,
+    };
+    
+    await request.put('/dropbox/fs/hello?dir', {
+        options,
+    });
+    
+    t.ok(mkdir.calledWith(token, '/hello'), 'should call mkdir with token');
+    t.end();
+});
+
+test('restbox: put: response', async (t) => {
+    const stream = through2((chunk, enc, fn) => fn(null, chunk));
+    const createWriteStream = sinon
+        .stub()
+        .returns(stream);
+    
+    mockRequire('@cloudcmd/dropbox', {
+        createWriteStream,
+    });
+    
+    reRequire(pathPut);
+    const restbox = reRequire(pathRestbox);
+    
+    const {request} = reRequire('serve-once')(restbox);
+    
+    const token = 'hello'
+    const options = {
+        token,
+    };
+    
+    const {body} = await request.put('/dropbox/fs/hi.txt', {
+        body: 'hello',
+        options,
+    });
+    
+    t.equal(body, 'save: ok("/hi.txt")', 'should equal');
+    t.end();
+});
+
+test('restbox: put: createWriteStream', async (t) => {
+    const stream = through2((chunk, enc, fn) => fn(null, chunk));
+    const createWriteStream = sinon
+        .stub()
+        .returns(stream);
+    
+    mockRequire('@cloudcmd/dropbox', {
+        createWriteStream,
+    });
+    
+    reRequire(pathPut);
+    const restbox = reRequire(pathRestbox);
+    
+    const {request} = reRequire('serve-once')(restbox);
+    
+    const token = 'hello'
+    const options = {
+        token,
+    };
+    
+    await request.put('/dropbox/fs/hi.txt', {
+        body: 'hello',
+        options,
+    });
+    
+    t.ok(createWriteStream.calledWith(token, '/hi.txt'), 'should call createWriteStream');
+    t.end();
+});
+
+test('restbox: put: unzip', async (t) => {
+    const stream = through2((chunk, enc, fn) => fn(null, chunk));
+    const createWriteStream = sinon
+        .stub()
+        .returns(stream);
+    
+    mockRequire('@cloudcmd/dropbox', {
+        createWriteStream,
+    });
+    
+    reRequire(pathPut);
+    const restbox = reRequire(pathRestbox);
+    
+    const {request} = reRequire('serve-once')(restbox);
+    
+    const token = 'hello'
+    const options = {
+        token,
+    };
+    
+    const {body} = await request.put('/dropbox/fs/hi.txt?unzip', {
+        body: 'hello',
+        options,
+    });
+    
+    t.equal(body, 'incorrect header check', 'should equal');
+    t.end();
+});
+
